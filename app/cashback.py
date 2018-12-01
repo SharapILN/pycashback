19f94205c2b4c2d50a7528b3365143cbeea75c30
NailMukhametshin <nail.zubr@gmail.com>
2018-12-01 11:18:54 +0300

    Limit added

--- app/cashback.py
+++ app/cashback.py
@@ -1,5 +1,10 @@
 def cashback(amount):
     percent  = 0.05
-    return amount * percent
+    limit = 3_000
+    result =  amount * percent
+    if result > 3000:
+        return limit
+
+    return result
 
 
